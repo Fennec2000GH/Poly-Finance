21002625a83103c43c42ff0791ce62a068e02202
root <root@DESKTOP-R7MUAPV.localdomain>
2021-10-10 01:41:48 -0400

    transforms sample datasets to have one matching column

--- data_transformation.py
+++ data_transformation.py
@@ -6,6 +6,8 @@ df = pd.read_csv('sample data/2011_Retail_Sales_By_Town_All_NAICS_-_ARCHIVE.csv'
 pprint(df.head())
 
 df['Town'] = df['Municipality'].transform(func=lambda x: x.split()[0].capitalize())
+col = column=df.pop(item='Town')
+df.insert(loc=0, column='Town', value=col)
 pprint(df.head())
 
 df.to_csv(path_or_buf='sample data/2011_Retail_Sales_By_Town_All_NAICS_-_ARCHIVE.csv')
